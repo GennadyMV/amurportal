c6e0727c6a0eb52409ed8dd046bd6c708511f01d
Mikhail Gersimov <mgerasim@inbox.ru>
2015-02-17 15:20:31 +1000

    A0004

---
+++ amurportal/amurportal/Controllers/AjaxController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace amurportal.Controllers
+{
+    public class AjaxController : Controller
+    {
+        //
+        // GET: /Ajax/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hello()
+        {
+            Hydro.HydroService theHydro = new Hydro.HydroService();
+            string ss = theHydro.Hello("test");
+            ViewBag.ss = ss;
+            return View();               
+        }
+
+        public ActionResult GetSiteTypes()
+        {
+            Hydro.HydroService theHydro = new Hydro.HydroService();
+            Hydro.SiteType[] theSiteTypes = theHydro.GetSiteTypes();
+
+            ViewBag.SiteTypes = theSiteTypes;
+            return View();
+        }
+
+        public ActionResult GetSites()
+        {
+            Hydro.HydroService theHydro = new Hydro.HydroService();
+            
+            return View();
+        }
+    }
+}
