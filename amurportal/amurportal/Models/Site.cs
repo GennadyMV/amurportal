45a3e2b622b22753f68db128c3eb8ea22f9691a2
Mikhail Gersimov <mgerasim@inbox.ru>
2015-02-17 16:43:06 +1000

    A0009

---
+++ amurportal/amurportal/Models/Site.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace amurportal.Models
+{
+    [JsonObject]
+    public class Site
+    {
+        [JsonProperty("TypeId")]
+        public uint TypeId { get; set; }
+        [JsonProperty("TypeName")]
+        public string TypeName { get; set; }
+        [JsonProperty("TypeNameShort")]
+        public string TypeNameShort { get; set; }
+    }
+}
