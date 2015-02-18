fe0c4bb7b5ef21b208877fae77c75e9c8b42c389
Mikhail Gersimov <mgerasim@inbox.ru>
2015-02-18 10:45:31 +1000

    A0009

--- amurportal/amurportal/Controllers/AjaxController.cs
+++ amurportal/amurportal/Controllers/AjaxController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using amurportal.Hydro;
 
 namespace amurportal.Controllers
 {
@@ -34,11 +35,52 @@ namespace amurportal.Controllers
             return View();
         }
 
-        public ActionResult GetSites()
+        public JsonResult GetSites()
         {
+            List<amurportal.Models.Site> theSites = new List<amurportal.Models.Site>();
+
             Hydro.HydroService theHydro = new Hydro.HydroService();
-            
-            return View();
+            Hydro.SiteType[] theSiteTypes = theHydro.GetSiteTypes();
+            foreach (var item in theSiteTypes)
+            {
+                Hydro.Site[] _sites = theHydro.GetSiteList(item.Id, true);
+                if (_sites == null) break;
+                foreach (var site in _sites)
+                {                    
+                    amurportal.Models.Site _site = new amurportal.Models.Site();
+                    _site.TypeId = item.Id;
+                    _site.TypeName = item.Name;
+                    _site.TypeNameShort = item.ShortName;
+                    _site.Border = site.Border;
+                    _site.Comment = site.Comment;                    
+                    _site.Id = site.Id;
+                    if (site.Lat != null)
+                    {
+                        _site.Lat = (decimal)site.Lat;
+                    }
+                    else
+                    {
+                        _site.Lat = 0;
+                    }
+                    if (site.Lon != null)
+                    {
+                        _site.Lon = (decimal)site.Lon;
+                    }
+                    else
+                    {
+                        _site.Lon = 0;
+                    }
+
+                    _site.Name = site.Name;                    
+                    _site.SiteCode = site.SiteCode;
+                    _site.SiteId = site.SiteId;
+                    _site.UtcOffset = site.UtcOffset;
+
+                    theSites.Add(_site);
+                }
+            }
+
+            return Json( theSites, JsonRequestBehavior.AllowGet ); 
         }
     }
 }
--- amurportal/amurportal/Models/Site.cs
+++ amurportal/amurportal/Models/Site.cs
@@ -10,10 +10,42 @@ namespace amurportal.Models
     public class Site
     {
         [JsonProperty("TypeId")]
-        public uint TypeId { get; set; }
+        public int TypeId { get; set; }
         [JsonProperty("TypeName")]
         public string TypeName { get; set; }
         [JsonProperty("TypeNameShort")]
         public string TypeNameShort { get; set; }
+        [JsonProperty("Border")]
+        public string Border { get; set; }
+        [JsonProperty("Comment")]
+        public string Comment { get; set; }
+        [JsonProperty("Height")]
+        public float Height { get; set; }
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+        [JsonProperty("Lat")]
+        public decimal Lat { get; set; }
+        [JsonProperty("LocalX")]
+        public float LocalX { get; set; }
+        [JsonProperty("LocalY")]
+        public float LocalY { get; set; }
+        [JsonProperty("Lon")]
+        public decimal Lon { get; set; }
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+        [JsonProperty("ParentSiteId")]
+        public int ParentSiteId { get; set; }
+        [JsonProperty("PosAccuracy_m")]
+        public float PosAccuracy_m { get; set; }
+        [JsonProperty("Ref")]
+        public string Ref { get; set; }
+        [JsonProperty("RegionId")]
+        public int RegionId { get; set; }
+        [JsonProperty("SiteCode")]
+        public string SiteCode { get; set; }
+        [JsonProperty("SiteId")]
+        public int SiteId { get; set; }
+        [JsonProperty("UtcOffset")]
+        public float UtcOffset { get; set; }
     }
 }
