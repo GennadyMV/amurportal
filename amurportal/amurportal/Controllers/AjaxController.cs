a0b868b3d0194a539235a04dbad2bfa4fa70c93b
Mikhail Gersimov <mgerasim@inbox.ru>
2015-02-25 15:40:07 +1000

    A0018

--- amurportal/amurportal/Controllers/AjaxController.cs
+++ amurportal/amurportal/Controllers/AjaxController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using amurportal.Hydro;
+using System.Globalization;
 
 namespace amurportal.Controllers
 {
@@ -74,6 +75,52 @@ namespace amurportal.Controllers
             }
 
             return Json( theSites, JsonRequestBehavior.AllowGet ); 
+        }
+
+        public ActionResult GetAllDataValue(int SiteId, string utcDateDDMMYYYY)
+        {
+            Hydro.HydroService theHydro = new HydroService();
+            DateTime utcDate = DateTime.ParseExact(utcDateDDMMYYYY + " 00:00:00", "ddMMyyyy H:mm:ss", new CultureInfo("en-US"));
+
+            Hydro.Site[] _sites = theHydro.GetSiteList(null, true);
+            int siteTypeId = 0;
+            if (_sites != null)
+            {
+                foreach (var site in _sites)
+                {
+                    if (site.SiteId == SiteId)
+                    {
+                        siteTypeId = site.Type.Id;
+                    }
+                }
+            }
+
+            Hydro.Variable[] theVariables = theHydro.GetSiteTypeVariables(siteTypeId, true);
+
+            Dictionary<string, List<Hydro.DataValue>> dictionary_in = new Dictionary<string, List<Hydro.DataValue>>();
+            
+
+            foreach (var variable in theVariables)
+            {
+                Hydro.DataValue[] theDataValues = theHydro.GetAllDataValue(SiteId, false, utcDate, true, variable.Id, false, null, false, null, false);
+                if (theDataValues == null) continue;
+                List<Hydro.DataValue> theDataValuesList = new List<Hydro.DataValue>();
+                foreach (var value in theDataValues)
+                {
+                    theDataValuesList.Add(value);
+                }
+                dictionary_in.Add(variable.Name, theDataValuesList);                
+            }
+            ViewBag.Variables = theVariables;
+            ViewBag.dictionary_in = dictionary_in;            
+            return View();
+        }
+
+        public ActionResult GetSiteTypeVariables(int siteTypeId)
+        {
+            Hydro.HydroService theHydro = new HydroService();
+            Hydro.Variable[] theVariables = theHydro.GetSiteTypeVariables(siteTypeId, true);
+            return View();
         }
     }
 }
