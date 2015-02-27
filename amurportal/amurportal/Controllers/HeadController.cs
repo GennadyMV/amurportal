a16b9247a0e6874bbdb9ff073ee36a1f960c1d96
Mikhail Gersimov <mgerasim@inbox.ru>
2015-02-27 12:20:11 +1000

    A0021

---
+++ amurportal/amurportal/Controllers/HeadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace amurportal.Controllers
+{
+    public class HeadController : Controller
+    {
+        //
+        // GET: /Head/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetTypes()
+        {
+            Hydro.HydroService theHydro = new Hydro.HydroService();
+            Hydro.SiteType[] theSiteTypes = theHydro.GetSiteTypes();
+            ViewBag.SiteTypes = theSiteTypes;            
+            return View();
+        }
+
+        public ActionResult GetSites(int TypeId = 0)
+        {
+            Hydro.HydroService theHydro = new Hydro.HydroService();
+            Hydro.Site[] theSites;
+            if (TypeId > 0)
+            {
+                theSites = theHydro.GetSiteList(TypeId, true);
+            }
+            else
+            {
+                theSites = theHydro.GetSiteList(null, true);
+            }
+            ViewBag.Sites = theSites;
+
+            return View();
+        }
+
+    }
+}
